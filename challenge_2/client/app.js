b2037cf07684fbb488d6a976d95a04908bd0a5e6
Kelsy Vaughn <angelstate@gmail.com>
2020-03-03 16:05:04 -0800

    continue attempts to add input data to display

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,35 +1,15 @@
 
 window.addEventListener("load", function () {
   function sendData() {
-    const XHR = new XMLHttpRequest();
-
-    // Bind the FormData object and the form element
-    const FD = new FormData(form);
-
-    // Define what happens on successful data submission
-    XHR.addEventListener("load", function (event) {
-      alert(event.target.responseText);
-    });
-
-    // Define what happens in case of error
-    XHR.addEventListener("error", function (event) {
-      alert('Oops! Something went wrong.');
-    });
-
-    // Set up our request
-    XHR.open("POST", " ");
-
-    // The data sent is what the user provided in the form
-    XHR.send(FD);
-  }
 
   // Access the form element...
-  let form = document.getElementById("userform");
+  let form = document.getElementById("form");
 
+  // const form = document.getElementById("form")
   // ...and take over its submit event.
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     sendData();
   });
-});
+}});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, '/client'))); //point express to client folder to connect html
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/', (req, res) => res.send('Send data to server'));
+app.post('/', (req, res) => {
+  res.send('this submission has been sent');
+  // res.end()
+});
 
-app.use(express.static('client'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
