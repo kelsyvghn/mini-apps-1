5f7cb82446e62c93e0a80483d9a25f32d47731d7
Kelsy Vaughn <angelstate@gmail.com>
2020-03-04 16:25:21 -0800

    not completed, babel would not load properly

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -6,9 +6,8 @@ const App = () => (
   <div>
     "Hello React App!"
   </div>
-)
+  )
+
 
 
 export default App;
-
-ReactDOM.render(<App />, document.getElementById('App'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const babel = require("@babel/core");
+// const babel = require("@babel/core");
 
-babel.transform("code", optionsObject);
-// const babel = require("@babel/core").transform("code", options);
+// babel.transform("code", optionsObject);
 app.use('/', express.static(path.join(__dirname, '/client'))); //point express to client folder to connect html
+// const babel = require("@babel/core").transform("code", options);
 // app.use(express.json());
 
 // app.use(express.urlencoded({ extended: false })) app.get('/', (req, res) => res.send('Hello World!'));
