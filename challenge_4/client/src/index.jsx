47a92d04e6e5d4ff17f73969925e9aa5c4e47ae8
Kelsy Vaughn <angelstate@gmail.com>
2020-03-05 10:55:21 -0800

    front end initial set up, react App component, and index.html setup

---
+++ challenge_4/client/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+console.log('HELLO KELSY!');
+
+ReactDOM.render(<App/>, document.getElementById('app'));
